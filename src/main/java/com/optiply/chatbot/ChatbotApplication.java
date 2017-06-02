618c73c1519e590f225850f48f40f257e06fa3e2
D.F. Duwaer <davidduwaer@gmail.com>
2017-06-02 12:41:43 +0200

    initial commit

---
+++ src/main/java/com/optiply/chatbot/ChatbotApplication.java
@@ -0,0 +1,12 @@
+package com.optiply.chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatbotApplication.class, args);
+	}
+}
